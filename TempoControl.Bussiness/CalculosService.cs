94e178929399f5e3d4f5f1df348125a3929dc3f0
Yveguerrerob-404 <yveguerrerob@gmail.com>
2026-04-23 11:33:43 -0400

    Add files via upload

---
+++ TempoControl.Bussiness/CalculosService.cs
@@ -0,0 +1,21 @@
+﻿namespace TempoControl.Business;
+
+public class CalculosService {
+    public double ObtenerTotalHoras(DateTime entrada, DateTime salida) {
+        // Restamos la salida menos la entrada para obtener el tiempo
+        TimeSpan diferencia = salida - entrada;
+        
+        // Retornamos el total de horas 
+        return diferencia.TotalHours;
+    }
+
+    // Para el reporte mensual
+    // se suman las horas de varios días
+    public double SumarHorasDelMes(List<double> listaDeHorasDiarias) {
+        double totalMes = 0;
+        foreach (var hora in listaDeHorasDiarias) {
+            totalMes += hora;
+        }
+        return totalMes;
+    }
+}
